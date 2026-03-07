fbefc5b1ffb0a58c5e922aefeb59f0e2a9c3162d
Angad Bhardwaj <angadb237@gmail.com>
2026-03-07 19:38:46 +0530

    Add queue implementation in DAY 35 Q1.C

---
+++ #DSA_CODES/DAY 35 Q1.C
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+int queue[MAX];
+int front = -1, rear = -1;
+
+void enqueue(int x)
+{
+    if(rear == MAX-1) return;
+    if(front == -1) front = 0;
+    queue[++rear] = x;
+}
+
+void display()
+{
+    for(int i = front; i <= rear; i++)
+        printf("%d ", queue[i]);
+}
+
+int main()
+{
+    int n, x;
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &x);
+        enqueue(x);
+    }
+    display();
+    return 0;
+}
