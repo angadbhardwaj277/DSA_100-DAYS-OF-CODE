4a45cda73411a1d39f87f9d38da993d7e24793a0
Angad Bhardwaj <angadb237@gmail.com>
2026-03-14 21:50:11 +0530

    Add queue and stack implementation in C

---
+++ #DSA_CODES/DAY 42 Q1.C
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+int queue[MAX], stack[MAX];
+int front = -1, rear = -1, top = -1;
+
+void enqueue(int x){
+    if(rear == MAX-1) return;
+    if(front == -1) front = 0;
+    queue[++rear] = x;
+}
+
+int dequeue(){
+    if(front == -1 || front > rear) return -1;
+    return queue[front++];
+}
+
+void push(int x){
+    stack[++top] = x;
+}
+
+int pop(){
+    return stack[top--];
+}
+
+int main(){
+    int n,x,i;
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++){
+        scanf("%d",&x);
+        enqueue(x);
+    }
+
+    while(front<=rear){
+        push(dequeue());
+    }
+
+    while(top!=-1){
+        enqueue(pop());
+    }
+
+    for(i=front;i<=rear;i++){
+        printf("%d ",queue[i]);
+    }
+
+    return 0;
+}
