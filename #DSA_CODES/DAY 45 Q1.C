9ce667c3763639fd03e6118fb7634c338e13d5b9
Angad Bhardwaj <angadb237@gmail.com>
2026-03-17 19:59:38 +0530

    Create DAY 45 Q1.C

---
+++ #DSA_CODES/DAY 45 Q1.C
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node *left, *right;
+} Node;
+
+Node* createNode(int data) {
+    Node* n = (Node*)malloc(sizeof(Node));
+    n->data = data;
+    n->left = n->right = NULL;
+    return n;
+}
+
+int height(Node* root) {
+    if (!root) return 0;
+    int l = height(root->left);
+    int r = height(root->right);
+    return (l > r ? l : r) + 1;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+    if (n == 0 || arr[0] == -1) {
+        printf("0");
+        return 0;
+    }
+
+    Node* root = createNode(arr[0]);
+    Node* queue[n];
+    int front = 0, rear = 0;
+    queue[rear++] = root;
+
+    int i = 1;
+    while (i < n) {
+        Node* curr = queue[front++];
+
+        if (arr[i] != -1) {
+            curr->left = createNode(arr[i]);
+            queue[rear++] = curr->left;
+        }
+        i++;
+
+        if (i < n && arr[i] != -1) {
+            curr->right = createNode(arr[i]);
+            queue[rear++] = curr->right;
+        }
+        i++;
+    }
+
+    printf("%d", height(root));
+    return 0;
+}
