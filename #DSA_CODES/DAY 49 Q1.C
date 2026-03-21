978d893cd9bbb1b303f96d343a8d8a72aeeed5e5
Angad Bhardwaj <angadb237@gmail.com>
2026-03-21 23:14:42 +0530

    Create DAY 49 Q1.C

---
+++ #DSA_CODES/DAY 49 Q1.C
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+struct Node* insert(struct Node* root, int key) {
+    if (root == NULL) return newNode(key);
+    if (key < root->data)
+        root->left = insert(root->left, key);
+    else if (key > root->data)
+        root->right = insert(root->right, key);
+    return root;
+}
+
+void inorder(struct Node* root) {
+    if (root == NULL) return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+int main() {
+    int n, x;
+    scanf("%d", &n);
+    struct Node* root = NULL;
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        root = insert(root, x);
+    }
+    inorder(root);
+    return 0;
+}
