570bdc731e3653181305d3b296ade193796c97e1
Angad Bhardwaj <angadb237@gmail.com>
2026-03-23 23:15:52 +0530

    Create DAY 51 Q1.C

---
+++ #DSA_CODES/DAY 51 Q1.C
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+struct TreeNode* createNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+struct TreeNode* insert(struct TreeNode* root, int val) {
+    if (root == NULL) return createNode(val);
+    if (val < root->val)
+        root->left = insert(root->left, val);
+    else
+        root->right = insert(root->right, val);
+    return root;
+}
+
+struct TreeNode* LCA(struct TreeNode* root, int p, int q) {
+    if (root == NULL) return NULL;
+    if (p < root->val && q < root->val)
+        return LCA(root->left, p, q);
+    if (p > root->val && q > root->val)
+        return LCA(root->right, p, q);
+    return root;
+}
+
+int main() {
+    int n, x, p, q;
+    scanf("%d", &n);
+
+    struct TreeNode* root = NULL;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        root = insert(root, x);
+    }
+
+    scanf("%d %d", &p, &q);
+
+    struct TreeNode* ans = LCA(root, p, q);
+    if (ans != NULL)
+        printf("%d", ans->val);
+
+    return 0;
+}
